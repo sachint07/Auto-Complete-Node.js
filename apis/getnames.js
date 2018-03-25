198794772d61d56dfff3ff40aaa125487195d63e
sachin1.talekar <sachin1.talekar@ril.com>
2018-03-26 01:10:41 +0530

    added seperate scripts folder

--- apis/getnames.js
+++ apis/getnames.js
@@ -20,19 +20,20 @@ Router.get("/get/:searchedTerm", (request, reply) => {
 
 function fetchNames(searchedTerm, callbackFetchNames) {
 
-    R.db.redis.zrank("names-Females-A", searchedTerm).then((resultsRank) => {
-        console.log(`-- > ${resultsRank}`);
-        if(resultsRank==null){
-            return callbackFetchNames("error", null);
-        }
-        let count = resultsRank + 50;
-        R.db.redis.zrange("names-Females-A", resultsRank, count)
-            .then((resultsRange) => {
-                return callbackFetchNames(null, resultsRange)
-            })
-    }).catch((e) => {
-        return callbackFetchNames(e, null);
-    })
+    R.db.redis.zrank("names-Females-A", searchedTerm)
+        .then((resultsRank) => {
+            console.log(`-- > ${resultsRank}`);
+            if (resultsRank == null) {
+                return callbackFetchNames("error", null);
+            }
+            let count = resultsRank + 50;
+            R.db.redis.zrange("names-Females-A", resultsRank, count)
+                .then((resultsRange) => {
+                    return callbackFetchNames(null, resultsRange)
+                })
+        }).catch((e) => {
+            return callbackFetchNames(e, null);
+        })
 }
 
 module.exports = Router;
